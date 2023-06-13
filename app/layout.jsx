d6c44d950222f51c898b6259fc7def2c5bd4ce1a
roms <rusirusamaraweera749@gmail.com>
2023-06-13 17:55:23 +0530

    create landing page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,28 @@
-import '@styles/globals.css';
+import Provider from '@components/Provider';
+import '../styles/globals.css';
+import Nav from '@components/Nav';
 
 export const metadata = {
-    title: 'Promptopia',
-    description: 'Discover & Share AI Prompts'
-}
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts',
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
-    <html long="en">
-        <body>
-            <div className="main">
-                <div className="gradient">
-
-                </div>
-            </div>
-            <main className="app">
-                {children}
-            </main>
-        </body>
+    <html long='en'>
+      <body>
+        <Provider>
+          <div className='main'>
+            <div className='gradient'></div>
+          </div>
+          <main className='app'>
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
