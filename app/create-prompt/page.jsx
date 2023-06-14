d8867460d591b9553af77d1b74047b5f7240d271
roms <rusirusamaraweera749@gmail.com>
2023-06-14 17:54:56 +0530

    update prmopt route

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/router";
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
@@ -12,7 +12,27 @@ const CreatePrompt = () => {
     })
 
     const createPrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
 
+        try {
+            const response = await fetch('/api/prompt/new', 
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            })
+            if(response.ok) {
+                Router.push('/');
+            }
+        }catch(error) {
+            console.log(error);
+        }finally {
+            setSubmitting(false);
+        }
     }
 
   return (
